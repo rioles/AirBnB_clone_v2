d3e872282217ff6999ec2363a776a75672e04684
rioles <rioles1992@gmail.com>
2022-10-14 10:29:27 +0100

    update

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -33,7 +33,7 @@ def accept_only_int(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def rendering_template(n):
+def numbersandtemplates(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
