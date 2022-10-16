bc2dea0073adc0586e45d2b39c362cb66874d8ac
rioles <rioles1992@gmail.com>
2022-10-16 16:58:41 +0100

    state by id

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,7 +12,7 @@ app = Flask("__name__")
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """display the states and cities listed in alphabetical order"""
-    states = storage.all("City").values()
+    states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
