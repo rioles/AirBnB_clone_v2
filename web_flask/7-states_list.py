7834deb27917ad661f4ef94bbb4910c54cf98a81
rioles <rioles1992@gmail.com>
2022-10-16 14:55:36 +0100

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,7 +8,7 @@ from models import storage
 from flask import Flask
 from flask import render_template
 
-app = Flask(__name__)
+app = Flask("__name__")
 
 
 @app.route("/states_list", strict_slashes=False)
